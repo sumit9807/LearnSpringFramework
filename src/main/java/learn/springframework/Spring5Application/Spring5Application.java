fc27dd1b09ea6a58e14f1e3c1893649fe1762903
SUMIT SINGH <singhsumit28lko@gmail.com>
2021-12-05 20:08:55 +0530

    Initial Commit

---
+++ src/main/java/learn/springframework/Spring5Application/Spring5Application.java
@@ -0,0 +1,14 @@
+package learn.springframework.Spring5Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5Application.class, args);
+		System.out.println("Test");
+	}
+
+}
---
+++ src/test/java/learn/springframework/Spring5Application/Spring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.springframework.Spring5Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
